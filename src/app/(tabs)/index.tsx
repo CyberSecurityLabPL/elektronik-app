bc0b7b818286127d3be5baff68832536b1f421e1
Stanislav Mamrenko <s.mamrenko@perceptus.pl>
2024-11-06 15:28:23 +0100

    feat(component): event-card

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,25 @@
 import ScreenWrapper from "@/components/ScreenWrapper"
 import { Text } from "react-native"
+import Event from "@/components/Event/Event"
 
 export default function TabOneScreen() {
 	return (
-		<ScreenWrapper>
-			<Text>Homepage</Text>
+		<ScreenWrapper className="flex justify-center items-center gap-4
+		">
+			<Event
+			type="event"
+			title="Zawody szkolne"
+			content="Mocarny Elektroniaż"
+			date="12 czerwca"
+			timeLeft="Za 3 dni"
+			 />
+			<Event
+			type="future event"
+			title="Zawody szkolne"
+			content="Mocarny Elektroniaż"
+			date="12 czerwca"
+			timeLeft="Za 3 dni"
+			 />
 		</ScreenWrapper>
 	)
 }
