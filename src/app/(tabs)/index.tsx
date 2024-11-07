1520f7b8c01e8698a992aafe1f6d94b46d66e404
tkhtqe <stasmamrenko4@gmail.com>
2024-11-07 15:07:30 +0100

    feet(component): lesson-card

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,17 @@
 import ScreenWrapper from "@/components/ScreenWrapper"
 import { Text } from "react-native"
+import LessonCard from "@/components/plan/LessonCard"
 
 export default function TabOneScreen() {
 	return (
-		<ScreenWrapper>
-			<Text>Homepage</Text>
+		<ScreenWrapper className="flex justify-center items-center">
+			<LessonCard
+				period={1}
+				time="8:00-8:45"
+				subject="Matematyka"
+				teacher="AA"
+				room="101"
+			/>
 		</ScreenWrapper>
 	)
 }
