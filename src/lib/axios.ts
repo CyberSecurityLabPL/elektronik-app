59ea227c9032de50d204bc5276eadbb0dad37ce8
Pullolo <pllwot89@gmail.com>
2024-11-12 09:38:12 +0100

    feat: news and substitutions requests and their types

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,52 @@
+import {
+	NewsResponse,
+	SingleNewsResponse,
+	SubstitutionsResponse,
+} from "@/types/axios"
 import axios from "axios"
+import { format } from "date-fns"
 
 export const api = axios.create({
-	baseURL: "https://jsonplaceholder.typicode.com",
+	baseURL: "http://api.elektronik.zgora.pl/api",
 	headers: {
 		"Content-Type": "application/json",
 	},
 })
+
+export async function getSubstitutions(
+	date: Date,
+): Promise<SubstitutionsResponse | undefined> {
+	try {
+		const data = await api.get(
+			`/substitutions?pagination[page]=1&pagination[pageSize]=1&sort[1]=createdAt:desc&filters[date][$eq]=${format(
+				date,
+				"yyyy-MM-dd",
+			)}`,
+		)
+
+		return data.data
+	} catch (error) {}
+}
+
+export async function getNews(
+	page: number,
+	pageSize: number,
+): Promise<NewsResponse | undefined> {
+	try {
+		const data = await api.get(
+			`/articles?pagination[pageSize]=${pageSize}&pagination[page]=${page}`,
+		)
+
+		return data.data
+	} catch (error) {}
+}
+
+export async function getArticle(
+	id: number,
+): Promise<SingleNewsResponse | undefined> {
+	try {
+		const data = await api.get(`/articles/${id}`)
+
+		return data.data
+	} catch (error) {}
+}
---
+++ src/types/axios.ts
@@ -0,0 +1,74 @@
+export interface StrapiImage {
+	data: {
+		id: number
+		attributes: {
+			name: string
+			alternativeText: string | null
+			caption: string | null
+			width: number
+			height: number
+			formats: any
+			hash: string
+			ext: string
+			mime: string
+			size: number
+			url: string
+			previewUrl: string | null
+			provider: string
+			provider_metadata: any
+			createdAt: string | Date
+			updatedAt: string | Date
+		}
+	}
+}
+
+export interface StrapiArticle {
+	id: number
+	attributes: {
+		title: string
+		description: string
+		content: string
+		customDate: string | Date | null
+		createdAt: string | Date
+		publishedAt: string | Date
+		image: StrapiImage
+	}
+}
+
+export interface SubstitutionsResponse {
+	data: {
+		id: number
+		attributes: {
+			date: string
+			substitutions: string
+			createdAt: string | Date
+			updatedAt: string | Date
+			publishedAt: string | Date
+		}
+	}[]
+	meta: {
+		pagination: {
+			page: number
+			pageSize: number
+			pageCount: number
+			total: number
+		}
+	}
+}
+
+export interface NewsResponse {
+	data: StrapiArticle[]
+	meta: {
+		pagination: {
+			page: number
+			pageSize: number
+			pageCount: number
+			total: number
+		}
+	}
+}
+
+export interface SingleNewsResponse {
+	data: StrapiArticle
+	meta: {}
+}
