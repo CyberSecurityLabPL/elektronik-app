6516e36aa88f517fbfafc0a5ae9633a146d84275
Verti1234 <krzysztofgodyn501@gmail.com>
2024-11-13 15:31:33 +0100

    in-progress: data fetch

--- src/app/(drawer)/(tabs)/(news)/index.tsx
+++ src/app/(drawer)/(tabs)/(news)/index.tsx
@@ -1,15 +1,106 @@
+import NewsCard from "@/components/cards/NewsCard"
 import ScreenWrapper from "@/components/ScreenWrapper"
-import { Link } from "expo-router"
-import { Text } from "react-native"
-import Event from "@/components/Event/Event"
+import Heading from "@/components/ui/Heading"
+import { cn } from "@/lib/utils"
+import { router } from "expo-router"
+import { useEffect, useRef, useState } from "react"
+import { Animated, Pressable, Text, View, Dimensions } from "react-native"
 
 const News = () => {
+  const [activeTab, setActiveTab] = useState(0)
+  const slideAnim = useRef(new Animated.Value(0)).current
+
+  const containerWidth = Dimensions.get("window").width - 24
+
+  const tabWidth = (containerWidth - 32) / 2
+
+  const MARGIN = 4
+  const START_POSITION = MARGIN
+  const END_POSITION = tabWidth + MARGIN
+
+  const animateSlide = (toValue: number) => {
+    Animated.spring(slideAnim, {
+      toValue: toValue === 0 ? START_POSITION : END_POSITION,
+      useNativeDriver: true,
+      tension: 80,
+      friction: 10,
+    }).start()
+  }
+
+  useEffect(() => {
+    animateSlide(activeTab)
+  }, [activeTab])
+
   return (
     <ScreenWrapper>
-      <Text>News page</Text>
-      <Link href="/(tabs)/(news)/42" asChild>
-        <Text className="text-white">Navigate to nested route</Text>
-      </Link>
+      <Heading title="Ogłoszenia" />
+      <View className="bg-background-secondary h-16 rounded-2xl flex flex-row justify-around items-center px-2 relative overflow-hidden mt-4">
+        {/* Animated Background */}
+        <Animated.View
+          style={[
+            {
+              position: "absolute",
+              width: tabWidth - MARGIN * 2,
+              height: 48,
+              backgroundColor: "#6994ff",
+              borderRadius: 12,
+              transform: [{ translateX: slideAnim }],
+            },
+          ]}
+        />
+
+        {/* First Tab */}
+        <Pressable
+          onPress={() => setActiveTab(0)}
+          className="w-1/2 h-12 flex justify-center items-center"
+        >
+          <Text
+            className={cn(
+              activeTab == 0 ? "text-white" : "text-zinc-600",
+              "text-xl font-psemibold ",
+            )}
+          >
+            Szkolne
+          </Text>
+        </Pressable>
+
+        {/* Second Tab */}
+        <Pressable
+          onPress={() => setActiveTab(1)}
+          className="w-1/2 h-12 flex justify-center items-center"
+        >
+          <Text
+            className={cn(
+              activeTab == 1 ? "text-white" : "text-zinc-600",
+              "text-xl font-psemibold ",
+            )}
+          >
+            Samorządu
+          </Text>
+        </Pressable>
+      </View>
+
+      {/* Content */}
+      <View className="bg-background-secondary mt-4 min-h-full rounded-2xl p-4">
+        {activeTab == 0 ? (
+          <NewsCard
+            title="Szkolne"
+            description={"dsadsdadasd"}
+            isFeatured
+            date="21 czerwca"
+            onPress={() => router.push("/(tabs)/(news)/1")}
+            image="https://plus.unsplash.com/premium_photo-1683121584322-ecd840dd9c41?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        ) : (
+          <NewsCard
+            title="Samorządu"
+            description={"dsadsdadasd"}
+            isFeatured
+            date="21 czerwca"
+            image="https://images.unsplash.com/photo-1721332150382-d4114ee27eff?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        )}
+      </View>
     </ScreenWrapper>
   )
 }
--- src/app/(drawer)/(tabs)/substitutions.tsx
+++ src/app/(drawer)/(tabs)/substitutions.tsx
@@ -1,10 +1,91 @@
 import ScreenWrapper from "@/components/ScreenWrapper"
-import { Text } from "react-native"
+import Heading from "@/components/ui/Heading"
+import IconButton from "@/components/ui/IconButton"
+import { addDays, format, subDays } from "date-fns"
+import { ChevronLeft, ChevronRight } from "lucide-react-native"
+import { RefreshControl, ScrollView, Text, View } from "react-native"
+import { pl } from "date-fns/locale/pl"
+import { useCallback, useEffect, useState } from "react"
+
+import { useSubstitutions } from "@/hooks/substitutions/useSubstitutions"
+import useColors from "@/hooks/useColors"
 
 const Substitutions = () => {
+  const [date, setDate] = useState(new Date())
+  const [refreshing, setRefreshing] = useState(false)
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true)
+    setTimeout(() => {
+      setRefreshing(false)
+      refetch()
+    }, 2000)
+  }, [])
+
+  const goBack = () => {
+    setDate((prevDate) => subDays(prevDate, 1))
+  }
+
+  const goForward = () => {
+    setDate((prevDate) => addDays(prevDate, 1))
+  }
+
+  useEffect(() => {
+    const { data, isLoading, isError, refetch, isRefetching } =
+      useSubstitutions({
+        date,
+      })
+  }, [date])
+
+  const colors = useColors()
+
   return (
     <ScreenWrapper>
-      <Text>Substitutions page</Text>
+      <ScrollView
+        className="  flex flex-col"
+        contentContainerClassName="justify-center"
+        nestedScrollEnabled={true}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        <Heading title="Zastępstwa" />
+        <View className="bg-background-secondary w-full p-4 rounded-2xl mt-8 flex justify-center items-center">
+          <Text className="text-3xl font-pmedium text-foreground">
+            {format(date, "d MMMM yyyy", { locale: pl })}
+          </Text>
+        </View>
+        <View className="bg-background-secondary light w-full p-4 rounded-2xl mt-6   ">
+          <View className="max-h-96 h-96 min-h-96  bg-background rounded-lg p-6">
+            <ScrollView nestedScrollEnabled>
+              <Text className="text-foreground text-base font-pregular">
+                {isError && "Błąd"}
+                {(isLoading || isRefetching) && (
+                  <View className="rounded-full bg-slate-800 w-full h-4 animate-pulse"></View>
+                )}
+                {data?.data.length === 0 ? (
+                  <View></View>
+                ) : (
+                  data?.data[0].attributes.substitutions
+                )}
+              </Text>
+            </ScrollView>
+          </View>
+        </View>
+
+        <View className="flex flex-row justify-around items-center w-full mt-4 ">
+          <IconButton
+            onPress={goBack}
+            LucideIcon={ChevronLeft}
+            iconColor={colors.foreground}
+          />
+          <IconButton
+            onPress={goForward}
+            LucideIcon={ChevronRight}
+            iconColor={colors.foreground}
+          />
+        </View>
+      </ScrollView>
     </ScreenWrapper>
   )
 }
