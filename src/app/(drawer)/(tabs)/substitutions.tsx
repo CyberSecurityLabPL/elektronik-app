ab888b9662f8a8c5c92811445564e36431da60dc
Verti1234 <krzysztofgodyn501@gmail.com>
2024-11-14 10:04:14 +0100

    feat(screen): add substitutions screen

--- src/app/(drawer)/(tabs)/substitutions.tsx
+++ src/app/(drawer)/(tabs)/substitutions.tsx
@@ -1,43 +1,45 @@
 import ScreenWrapper from "@/components/ScreenWrapper"
 import Heading from "@/components/ui/Heading"
 import IconButton from "@/components/ui/IconButton"
-import { addDays, format, subDays } from "date-fns"
-import { ChevronLeft, ChevronRight } from "lucide-react-native"
-import { RefreshControl, ScrollView, Text, View } from "react-native"
-import { pl } from "date-fns/locale/pl"
-import { useCallback, useEffect, useState } from "react"
-
 import { useSubstitutions } from "@/hooks/substitutions/useSubstitutions"
 import useColors from "@/hooks/useColors"
+import { useQueryClient } from "@tanstack/react-query"
+import { addDays, format, subDays } from "date-fns"
+import { pl } from "date-fns/locale/pl"
+import { ChevronLeft, ChevronRight } from "lucide-react-native"
+import { useCallback, useState } from "react"
+import { RefreshControl, ScrollView, Text, View } from "react-native"
 
 const Substitutions = () => {
   const [date, setDate] = useState(new Date())
   const [refreshing, setRefreshing] = useState(false)
 
+  const queryClient = useQueryClient()
+  const colors = useColors()
+
+  const { data, isLoading, isError, isRefetching, fetchNextPage, isFetching } =
+    useSubstitutions({ date })
+
   const onRefresh = useCallback(() => {
     setRefreshing(true)
-    setTimeout(() => {
-      setRefreshing(false)
-      refetch()
-    }, 2000)
+    queryClient
+      .invalidateQueries({ queryKey: ["substitutions"] })
+      .finally(() => {
+        setRefreshing(false)
+      })
+  }, [queryClient])
+
+  const goBack = useCallback(() => {
+    setDate((prevDate) => subDays(prevDate, 1))
+
+    fetchNextPage()
   }, [])
 
-  const goBack = () => {
-    setDate((prevDate) => subDays(prevDate, 1))
-  }
-
-  const goForward = () => {
+  const goForward = useCallback(() => {
     setDate((prevDate) => addDays(prevDate, 1))
-  }
 
-  useEffect(() => {
-    const { data, isLoading, isError, refetch, isRefetching } =
-      useSubstitutions({
-        date,
-      })
-  }, [date])
-
-  const colors = useColors()
+    fetchNextPage()
+  }, [])
 
   return (
     <ScreenWrapper>
@@ -46,7 +48,13 @@ const Substitutions = () => {
         contentContainerClassName="justify-center"
         nestedScrollEnabled={true}
         refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            tintColor={colors.primary}
+            colors={[colors.primary]}
+            progressBackgroundColor={colors.backgroundSecondary}
+          />
         }
       >
         <Heading title="Zastępstwa" />
@@ -58,17 +66,30 @@ const Substitutions = () => {
         <View className="bg-background-secondary light w-full p-4 rounded-2xl mt-6   ">
           <View className="max-h-96 h-96 min-h-96  bg-background rounded-lg p-6">
             <ScrollView nestedScrollEnabled>
-              <Text className="text-foreground text-base font-pregular">
-                {isError && "Błąd"}
-                {(isLoading || isRefetching) && (
-                  <View className="rounded-full bg-slate-800 w-full h-4 animate-pulse"></View>
+              <View>
+                {isError && (
+                  <Text className="text-foreground text-base font-pregular ">
+                    Wystąpił błąd, Odśwież lub spróbuj ponownie później
+                  </Text>
                 )}
-                {data?.data.length === 0 ? (
-                  <View></View>
+                {isLoading || isRefetching || isFetching ? (
+                  <View className="flex flex-col gap-2">
+                    <View className="rounded-full flex bg-zinc-500 w-full h-4 animate-pulse"></View>
+                    <View className="rounded-full flex bg-zinc-500 w-1/2 h-4 animate-pulse"></View>
+                    <View className="rounded-full flex bg-zinc-500 w-2/3 h-4 animate-pulse"></View>
+                  </View>
                 ) : (
-                  data?.data[0].attributes.substitutions
+                  <Text className="text-foreground text-base font-pregular">
+                    {data?.pages[0].data.length === 0 ? (
+                      <Text className="text-2xl font-psemibold text-foreground text-center">
+                        Brak Zastępstw
+                      </Text>
+                    ) : (
+                      data?.pages[0].data[0].attributes.substitutions
+                    )}
+                  </Text>
                 )}
-              </Text>
+              </View>
             </ScrollView>
           </View>
         </View>
--- src/hooks/substitutions/useSubstitutions.ts
+++ src/hooks/substitutions/useSubstitutions.ts
@@ -1,13 +1,26 @@
 import { SUBSTITUTIONS_URL } from "@/constants/urls"
 import { api } from "@/lib/axios"
+import { useInfiniteQuery } from "@tanstack/react-query"
 import { SubstitutionsResponse } from "./types"
-import { useQuery } from "@tanstack/react-query"
 
 export const useSubstitutions = ({ date }: { date: Date }) =>
-  useQuery<SubstitutionsResponse>({
-    queryKey: ["substitutions"],
+  useInfiniteQuery<SubstitutionsResponse>({
+    queryKey: ["substitutions", date],
+    initialPageParam: 1,
     queryFn: async () => {
       const { data } = await api.get(SUBSTITUTIONS_URL(date))
       return data
     },
+    getNextPageParam: (lastPage) => {
+      const lastDate = lastPage.data[lastPage.data.length - 1]?.attributes.date
+      return lastDate ? new Date(lastDate) : undefined
+    },
+    getPreviousPageParam: (firstPage) => {
+      const firstDate = firstPage.data[0]?.attributes.date
+      return firstDate ? new Date(firstDate) : undefined
+    },
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    staleTime: Infinity,
+    placeholderData: (previousData) => previousData,
   })
