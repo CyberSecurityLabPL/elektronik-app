4abcda8da4ace6f632af3f9b453d5994d327dcdd
Mikołaj Szlosowski <pllwot89@gmail.com>
2024-11-21 14:08:12 +0100

    fix: add lib for on async storage changes

--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -1,15 +1,36 @@
-import { getStorageData, StorageKeys } from "@/lib/storage"
+import { getStorageData, storageEventEmitter, StorageKeys } from "@/lib/storage"
 import { UserData } from "@/types/app-data"
 import { useEffect, useState } from "react"
+import { EventEmitter } from "eventemitter3"
 
 export const useUserData = () => {
   const [data, setData] = useState<UserData | undefined>()
+
   useEffect(() => {
+    // Initial data fetch
     getStorageData(StorageKeys.userData).then((result) => {
       if (result.success) {
         setData(result.data)
       }
     })
+
+    // Listen for storage changes
+    const handleStorageChange = (newData: UserData) => {
+      setData(newData)
+    }
+
+    storageEventEmitter.on(
+      `storage:${StorageKeys.userData}`,
+      handleStorageChange,
+    )
+
+    // Cleanup subscription on component unmount
+    return () => {
+      storageEventEmitter.off(
+        `storage:${StorageKeys.userData}`,
+        handleStorageChange,
+      )
+    }
   }, [])
 
   return data
